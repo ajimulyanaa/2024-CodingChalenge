f00cfba958cb111e5be4d126c2864e46f9fa6e22
ajimulyanaa <ajimmulyana98@gmail.com>
2024-01-30 18:21:58 +0700

    day-4

---
+++ day-4-sentenceSmash.js
@@ -0,0 +1,9 @@
+// penggabungan kata dalam array
+function smash1(words) {
+  return words.join(' ');
+}
+
+console.log(smash1(['this', 'is', 'a', 'really', 'long', 'sentence']));
+
+const smash = (words) => words.join(' ');
+console.log(smash(['this', 'is', 'a', 'really', 'long', 'sentence']));
