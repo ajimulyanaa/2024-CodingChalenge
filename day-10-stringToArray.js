6b55445e2e9d1755d2e3973663a6fef97399affb
ajimulyanaa <ajimmulyana98@gmail.com>
2024-02-05 20:16:02 +0700

    day-10

---
+++ day-10-stringToArray.js
@@ -0,0 +1,6 @@
+//convert string ke array dengan split
+// array.split('')  parameter sebagai pemisah
+
+const stringToArray = (string) => string.split(' ');
+
+console.log(stringToArray('I love arrays they are my favorite'));
